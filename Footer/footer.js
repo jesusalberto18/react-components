8c53f92ae402a57a139bf2efa0e05639f9f28d40
Alberto <albertiae.digital@gmail.com>
2023-01-16 18:38:47 -0400

    Created footer.js

---
+++ Footer/footer.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>"A lil bit of frontend here... A lil bit of backend there... Some mobile and AI"</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © J.A. Hernández {year}</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/philtaboada"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/phil-taboada-6b2a07174/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
