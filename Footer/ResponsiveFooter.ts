009b2d5292e62df51b751a7ad96015230b401884
Alberto <albertiae.digital@gmail.com>
2023-11-02 11:18:08 -0400

    Created ResponsiveFooter.ts

---
+++ Footer/ResponsiveFooter.ts
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Footer } from 'react-bootstrap';
+
+const ResponsiveFooter = () => {
+  return (
+    <Footer>
+      <div className="container">
+        <p>Copyright &copy; 2023 Company Name</p>
+      </div>
+    </Footer>
+  );
+};
+
+export default ResponsiveFooter;
