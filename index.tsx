38a240bfa041bcd48f53c25b1f5a15fb89adb354
Alberto <albertiae.digital@gmail.com>
2023-11-03 12:25:46 -0400

    Rendered the elements in index.tsx

---
+++ index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ResponsiveButton from './Button';
+import ResponsiveFooter from './Footer';
+import ResponsiveNavbar from './Navbar';
+import main from './Main';
+import header from './Header';
+import headerCss from './header-css';
+import ReactDOM from 'react-dom';
+
+
+/*
+ *   Creating the root element
+ */
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+/*
+ *   Rendering elements
+ */
+
+root.render(
+	<>
+	 <ResponsiveNavbar />
+	 <ResponsiveButton />
+	 <ResponsiveFooter />
+	 <Main />
+	 <Header />
+	</>
+);
